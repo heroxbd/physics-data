5642425edb75f209b2240b28916ec952b9d951d3
Benda Xu <orv@tsinghua.edu.cn>
2023-07-18 11:16:58 +0800

    dirac.py: assert 到 np.all 的结果。

--- dirac.py
+++ dirac.py
@@ -16,15 +16,17 @@ def acommute(a, b):
     反对易子
     '''
     ab = np.einsum('ilm,jmn->ijln', a, b)
-    ab_T = np.transpose(ab, (1,0,2,3))
+    ab_T = np.transpose(ab, (1, 0, 2, 3))
     return ab + ab_T
 
-assert acommute(dirac, dirac) == 2 * np.einsum('ij,kl->ijkl', eta, np.eye(4))
+assert np.all(acommute(dirac, dirac) == 2 * np.einsum('ij,kl->ijkl', eta, np.eye(4)))
 
 gamma5 = 1j * dirac[0] @ dirac[1] @ dirac[2] @ dirac[3]
 
-assert gamma5.T == gamma5
-assert gamma5 @ gamma5 == np.eye(4)
-# acommute(gamma5[None, :, :], dirac) # 思考为什么不都是 0
+assert np.all(gamma5.T == gamma5)
+assert np.all(gamma5 @ gamma5 == np.eye(4))
+assert np.all(0 == np.einsum('ij, ajk -> aik', gamma5, dirac) +
+              np.einsum('aij, jk -> aik', dirac, gamma5))
 dirac_sub = np.einsum('ab, bij -> aij', eta, dirac)
-assert np.einsum('aij, bjk, ckl, alm -> bcim', dirac, dirac, dirac, dirac_sub) == 4 * np.einsum('ij,kl->ijkl', eta, np.eye(4))
+assert np.all(np.einsum('aij, bjk, ckl, alm -> bcim', dirac, dirac, dirac, dirac_sub) == 
+              4 * np.einsum('ij, kl -> ijkl', eta, np.eye(4)))
